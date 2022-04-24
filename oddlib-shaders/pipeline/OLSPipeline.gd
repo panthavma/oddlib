ba170be4dcd29507f4d22b53a051181d0f687c8f
Papa Pana <papapana@tutanota.com>
2022-04-24 13:40:21 +0200

    Improved the pipeline, fixed some bugs

--- oddlib-shaders/pipeline/OLSPipeline.gd
+++ oddlib-shaders/pipeline/OLSPipeline.gd
@@ -1,7 +1,24 @@
 extends ViewportContainer
-# Still really early and lacks most of the functionnality.
+# TODO params
+# TODO redo pi and aquarelle in this
+# TODO Auto debug mode
+# TODO stop render
+# TODO add PG fixed buffer
+# todo note: texelfetch
+# TODO Refresh
 
 func Setup():
+	pass
+
+var _envPGBufferLit = preload("res://oddlib-shaders/pipeline/PGBufferLit-Environement.tres")
+
+
+func DebugGetName():
+	return "Unspecified"
+func DebugGetDebugModes():
+	return {"Normal View":[]}
+
+func OnRefresh():
 	pass
 
 
@@ -9,40 +26,75 @@ func Setup():
 
 # -------------
 
-func AddBuffer(bufferName, bufferType = "Unspecified"):
+func AddBuffer(bufferName, bufferParams = null):
 	var vp = Viewport.new()
 	vp.set_name(bufferName)
 	add_child(vp)
+	
+	_sizeChangedNodes.append(vp)
+	
+	var bufferType = _Get(bufferParams, "BufferType", "Unspecified")
+	var unshaded = _Get(bufferParams, "Unshaded", false)
+	vp.set_debug_draw((Viewport.DEBUG_DRAW_UNSHADED if unshaded else Viewport.DEBUG_DRAW_DISABLED))
+	
 	var bufInfo = {
-		"Name":bufferName, "Type":bufferType,
-		"Viewport":vp
+		"Name":bufferName, "BufferType":bufferType,
+		"Viewport":vp, "MaterialNode": _Get(bufferParams, "MaterialNode", null),
 	}
+	
 	_buffers[bufferName] = bufInfo
 	vp.set_handle_input_locally(false)
 	vp.set_update_mode(Viewport.UPDATE_ALWAYS)
 	vp.set_keep_3d_linear(true)
 	vp.set_hdr(false)
+	vp.set_transparent_background(true)
+	#vp.set_size_override(true, Vector2(1980, 1080))
+	vp.set_size_override(true, get_size())
 	return bufInfo
 
-func AddPGBuffer(bufferName, lit=true, layerMask = null):
-	var _buf = AddBuffer(bufferName, ("PGBuffer" if lit else "PGBufferUnshaded"))
-	var _cam = AddRenderCam(bufferName+"-Camera", bufferName, layerMask)
+func AddPGBuffer(bufferName, lit=true, bufferParams = null, camParams = null):
+	if(bufferParams == null):
+		bufferParams = {}
+	bufferParams["BufferType"] = _Get(bufferParams, "BufferType", ("PGBuffer" if lit else "PGBufferUnshaded"))
+	bufferParams["Unshaded"] = !lit
+	
+	if(camParams == null):
+		camParams = {}
+	camParams["Environment"] = _envPGBufferLit
+	
+	var _buf = AddBuffer(bufferName, bufferParams)
+	var _cam = AddRenderCam(bufferName+"-Camera", bufferName, camParams)
 
-func Add2DBuffer(_bufferName, _bufferMaterial, _bufferType="Generic 2D"): #TODO
-	pass
+func Add2DBuffer(bufferName, bufferMaterial, _bufferType="Generic 2D"): #TODO
+	var controlNode = ColorRect.new()
+	var buf = AddBuffer(bufferName, {"MaterialNode":controlNode})
+	buf["Viewport"].add_child(controlNode)
+	_sizeChangedNodes.append(controlNode)
+	controlNode.set_material(bufferMaterial)
 
 func AddCombinerBuffer(bufferName, _combinedBuffers, _combinerTexture = null): #TODO
 	# If texture == null; use alpha
 	var _buf = Add2DBuffer(bufferName, null, "Combiner Buffer")
 
-func AddRenderCam(cameraName, bufferName, _layerMask = null): #TODO
+func AddRenderCam(cameraName, bufferName, camParams = null): #TODO
 	var buf = GetBuffer(bufferName)
 	var cam = Camera.new()
 	cam.set_name(cameraName)
 	buf["Viewport"].add_child(cam)
 	
+	var fov = _Get(camParams, "FOV", 70)
+	cam.set_fov(fov)
+	
+	var cullMask = _Get(camParams, "CullMask", 0xFFFFF)
+	cam.set_cull_mask(cullMask)
+	
+	var environment = _Get(camParams, "Environment", null)
+	if(environment != null):
+		cam.set_environment(environment)
+	
 	var camInfo = {
-		"Camera":cam, "BufferName":bufferName
+		"Camera":cam, "BufferName":bufferName,
+		"FOV":fov, "CullMask":cullMask,
 	}
 	_cameras[cameraName] = camInfo
 	return camInfo
@@ -57,7 +109,15 @@ func AddParameter(parameterName, internalName, bufferName = null): # TODO
 	return paramInfo
 func SetParameter(parameterName, value):
 	var p = _params[parameterName]
-	var mat = get_material()
+	var targetNode = p["BufferName"]
+	if(targetNode == null):
+		targetNode = self
+	else:
+		targetNode = _buffers[targetNode]["MaterialNode"]
+		if(targetNode == null):
+			print("OddLib Shaders : SetParameter : targetNode is null ("+str(parameterName)+", "+str(value)+")")
+			return
+	var mat = targetNode.get_material()
 	mat.set_shader_param(p["InternalName"], value)
 
 func AddParameterVPTexture(parameterName, internalName, vpTexBufferName, bufferName = null):
@@ -72,12 +132,66 @@ func GetBuffer(bufferName):
 func GetCamera(cameraName):
 	return _cameras[cameraName]
 
-func DebugGetName():
-	return "Unspecified"
+func MoveCameras(globalTransform):
+	for cName in _cameras:
+		_cameras[cName]["Camera"].set_global_transform(globalTransform)
 
-func DebugGetDebugModes():
-	return {"Normal View":[]}
 
+
+# ------------------------------------------------------------------------------
+# Refresh
+
+signal OnRefreshOpportunity
+signal OnForceRefresh
+var _timeSinceLastRefresh = 0.0
+var _timeSinceLastForceRefresh = 0.0
+var _refreshOpportunityTargetTime = -1.0
+var _refreshForceTargetTime = -1.0
+
+
+func SetRefreshOpportunityFreq(newFreq = -1):
+	if(newFreq <= 0.0):
+		_refreshOpportunityTargetTime = -1.0
+	_refreshOpportunityTargetTime = 1.0/newFreq
+func SetForceRefreshFreq(newFreq = -1):
+	if(newFreq <= 0.0):
+		_refreshForceTargetTime = -1.0
+	_refreshForceTargetTime = 1.0/newFreq
+
+func RegisterModel(m):
+	var e1 = connect("OnRefreshOpportunity", m, "OnRefreshOpportunity")
+	var e2 = connect("OnForceRefresh", m, "OnForceRefresh")
+	if(e1 || e2):
+		print("OLSPipeline : Register model didn't manage to connect the signals.")
+
+func RefreshOpportunity():
+	OnRefreshOpportunity()
+	emit_signal("OnRefreshOpportunity")
+
+func OnRefreshOpportunity():
+	pass
+func OnForceRefresh():
+	pass
+
+func ForceRefresh():
+	OnForceRefresh()
+	emit_signal("OnForceRefresh")
+
+func _RefreshProcess(delta):
+	if(_refreshOpportunityTargetTime > 0 or _refreshForceTargetTime > 0):
+		_timeSinceLastRefresh += delta
+		_timeSinceLastForceRefresh += delta
+	
+	if(_refreshForceTargetTime > 0):
+		if(_timeSinceLastForceRefresh >= _refreshForceTargetTime):
+			_timeSinceLastRefresh = 0.0
+			_timeSinceLastForceRefresh = 0.0
+			ForceRefresh()
+	
+	if(_refreshOpportunityTargetTime > 0):
+		if(_timeSinceLastRefresh >= _refreshOpportunityTargetTime):
+			_timeSinceLastRefresh = 0.0
+			RefreshOpportunity()
 
 # --------------------------
 
@@ -85,29 +199,31 @@ var _buffers = {}
 var _cameras = {}
 var _params = {}
 
+var _sizeChangedNodes = []
+
+
+func _Get(dict, param, default):
+	if(dict != null and dict.has(param)):
+		return dict[param]
+	return default
+
+#func _init():
+#	OddLib.Pipeline = self
+
+func OnSizeChanged():
+	var screenSize = get_tree().get_root().get_size()
+	for n in _sizeChangedNodes:
+		n.set_size(screenSize)
 
 func _ready():
-	OddLib.Pipeline = self
-	#set_material(get_material().duplicate())
-	#get_material().setup_local_to_scene()
+	var err = get_tree().get_root().connect("size_changed", self, "OnSizeChanged")
+	if(err):
+		print("OLSPipeline : Couldn't connect to OnSizeChanged.")
 	set_stretch(true)
+	
 	Setup()
 	
-	# Needs a window resize signal to update the viewport hierarchy for some reason. TODO : Find a better way
-	var windowSize = OS.get_window_size()
-	_bootlegResizerSize = windowSize
-	OS.set_window_size(windowSize - Vector2(1,1))
-	_bootlegResizerTimer = 0.2
-	#OS.set_borderless_window(true)
-	#OS.set_window_fullscreen(true)
-	#OS.call_deferred("set_window_size",windowSize)
-	#call_deferred("set_size", Vector2(1.0,1.0), true)
-	#call_deferred("Setup")
+	OnSizeChanged()
 
-var _bootlegResizerTimer = 0.0
-var _bootlegResizerSize = Vector2(64,64)
 func _process(delta):
-	if(_bootlegResizerTimer > 0.0):
-		_bootlegResizerTimer -= delta
-		if(_bootlegResizerTimer <= 0.0):
-			OS.set_window_size(_bootlegResizerSize)
+	_RefreshProcess(delta)
